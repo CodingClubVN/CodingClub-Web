4f2e032d5fe439f2da7eef197c8d750fe630f9da
Thai Hoc Nguyen <thaihoc95x@gmail.com>
2021-07-09 15:33:12 +0700

    add function register

--- src/app/layout/register/register.component.ts
+++ src/app/layout/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { AuthService } from '../../share/services/auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,12 @@ import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  infoUser: any;
+  // @ts-ignore
+  infoUser: FormGroup;
 
-  constructor( private formBuilder: FormBuilder) { }
+  constructor( private formBuilder: FormBuilder,
+               private authService: AuthService,
+               private router: Router) { }
 
   ngOnInit(): void {
     this.infoUser = this.formBuilder.group({
@@ -19,6 +24,37 @@ export class RegisterComponent implements OnInit {
     });
   }
 
+  // tslint:disable-next-line:typedef
+  get f() {
+    return this.infoUser.controls;
+  }
+  checkPassword(): boolean{
+    console.log(this.f.confirmpassword.value);
+    if (this.f.confirmpassword.value === this.f.password.value){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
   onSubmit(): void{
+    if (this.checkPassword()){
+      const user = {
+        username: this.f.username.value,
+        password: this.f.password.value
+      };
+      console.log(user);
+      this.authService.register(user).subscribe(
+        res => {
+          console.log(res);
+          this.router.navigate(['/login']);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }else{
+      console.log('ERRR: Fail');
+    }
   }
 }
